72fad567e96e65f2e05f780a67956e89c73aaa3d
Victoria Gandolfi <victoria@ibureau.com.br>
2019-01-10 13:39:37 -0200

    Validação customizada: CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/cursomc/dto/ClienteDTO.java
@@ -14,7 +14,7 @@ public class ClienteDTO implements Serializable {
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
